52e55685bb30633d8483a19ff4cc37823e79aedd
Cheryl <cheryljfowlie@me.com>
2020-11-18 21:56:58 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
 // Dependencies
 var express = require("express");
 const fs = require("fs");
+const path = require('path');
 
 var app = express();
 const PORT = process.env.PORT || 8000;
+
+
+var notesArr = [];
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./public")));
+
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
+
+
+app.get("/api/notes", function (req, res) {
+    return res.sendFile(path.json(__dirname, "./db/db.json"));
+});
+
 
 // GET`/api/notes` - Should read the`db.json` file and return all saved notes as JSON.
 app.get("/api/notes", (req, res) => {
     fs.readFile(
         "./db/db.json",
         "utf8",
-        function (err, content) {
-            console.log(content);
-            res.json(JSON.parse(content));
+        function (err, notesArr) {
+            console.log(notesArr);
+            res.json(JSON.parse(notesArr));
         },
         function (err) {
             console.log(err);
