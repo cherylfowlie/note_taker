10c1858bc1f332be73d3f251119ca29f6150f09e
Cheryl <cheryljfowlie@me.com>
2021-01-14 21:21:29 +1100

    Update note taker

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 // Dependencies
 var express = require("express");
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 
 var app = express();
 const PORT = process.env.PORT || 8000;
 
-
-var notesArr = [];
+var notesData = [];
 
 // Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "./public")));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
@@ -23,11 +21,9 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-
 app.get("/api/notes", function (req, res) {
     return res.sendFile(path.json(__dirname, "./db/db.json"));
 });
-
 
 // GET`/api/notes` - Should read the`db.json` file and return all saved notes as JSON.
 app.get("/api/notes", (req, res) => {
@@ -46,24 +42,37 @@ app.get("/api/notes", (req, res) => {
 });
 
 // - POST`/api/notes` - Should receive a new note to save on the request body, add it to the`db.json` file, and then return the new note to the client.
-app.post('/api/notes', (req, res) => {
+///Post New Note///
+// writes the new note to the json file
+app.post("/api/notes", function (req, res) {
+    try {
+        // reads the json file
+        notesData = fs.readFileSync("./db/db.json", "utf8");
+        console.log(notesData);
 
-    const id = assignID();
-
-    fs.readFile('./db/db.json', 'utf8', function (err, notesArr) {
-        const notes = JSON.parse(notesArr);
-        notes.push({ id, ...req.body });
-
-        fs.writeFile('./db/db.json', JSON.stringify(notes), function (err) {
-            console.log(err);
+        // parse the data to get an array of objects
+        notesData = JSON.parse(notesData);
+        // Set new notes id
+        req.body.id = notesData.length;
+        // add the new note to the array of note objects
+        notesData.push(req.body); // req.body - user input
+        // make it string(stringify)so you can write it to the file
+        notesData = JSON.stringify(notesData);
+        // writes the new note to file
+        fs.writeFile("./db/db.json", notesData, "utf8", function (err) {
+            // error handling
+            if (err) throw err;
         });
-        console.log('Note Posted ' + id + notes);
-        res.json(notes);
+        // changeit back to an array of objects & send it back to the browser(client)
+        res.json(JSON.parse(notesData));
 
-    });
-
-
+        // error Handling
+    } catch (err) {
+        throw err;
+        console.error(err);
+    }
 });
+
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function () {
